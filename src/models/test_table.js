b8eae839c2823af33e83e1c84e977ca2e13f8330
Jakob Edelstein <jakobedelstein@gmail.com>
2024-04-21 06:33:16 -0600

    formatting

--- src/models/test_table.js
+++ src/models/test_table.js
@@ -3,15 +3,17 @@ import conn from '../services/db.js'
 function selectAllRows (req, res, next) {
 	const tableName = req.params[0].replace('-', '_')
 
-	conn.query(`SELECT * FROM ${tableName}`, (err, data, fields) => {
-		if (err) return next(new Error(err))
-		res.status(200).json({
-			status: 'success',
-			length: data?.length,
-			data: data,
-			controller: 'getAllRows'
-		})
-	})
+	conn.query(`SELECT * FROM ${tableName}`,
+		(err, data, fields) => {
+			if (err) return next(new Error(err))
+			res.status(200).json({
+				status: 'success',
+				length: data?.length,
+				data: data,
+				controller: 'getAllRows'
+			})
+		}
+	)
 }
 
 export {
